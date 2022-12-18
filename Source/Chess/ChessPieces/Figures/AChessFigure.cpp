2b26f2dd88546f94999553334f42bfa216fa6a0c
Piotr Penar <piotr.penar@dreamstormstudios.com>
2022-12-18 16:09:41 +0100

    Cleanup

--- Source/Chess/ChessPieces/Figures/AChessFigure.cpp
+++ Source/Chess/ChessPieces/Figures/AChessFigure.cpp
@@ -4,7 +4,6 @@
 void AChessFigure::GetAvailableMoves()
 {
 	const TArray<FMove> Moves = TArray<FMove>(SourcePiece->GetAvailableMoves());
+	
 	UE_LOG(LogTemp, Log, TEXT("Avaliable Moves: %d"), Moves.Num())
-	if(GEngine)
-		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Some debug message!"));	
 }
